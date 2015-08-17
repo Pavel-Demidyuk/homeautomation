d312795c6d4a3e8efb0506536807de78907cdf72
Pavel_Demidyuk <3128914@gmail.com>
2015-08-17 14:44:40 +0300

    shit -> gold

--- public/javascripts/modules/drives_install.js
+++ public/javascripts/modules/drives_install.js
@@ -56,31 +56,42 @@ define(['modules/drives_base', "jquery", "typeahead"], function (drivesBase, $) 
                 });
         },
 
-        bindSubmit: function() {
+        bindSubmit: function () {
             // submit
-            $("#installDrives").submit(function (event) {
-                io.emit("drives:save", $(this).serializeArray());
+            $("#installDrivesForm").submit(function (event) {
+                console.log($(this).serializeArray());
                 event.preventDefault();
+
+                return;
+                io.emit("drives:save", $(this).serializeArray());
             })
         },
 
-        bindSwitcherSensor: function() {
-            $('button[data-for="channel_type"]').each(function(e, element){
+        bindSwitcherSensor: function () {
+            $('button[data-for="channel_type"]').each(function (e, element) {
                 var driveId = drivesBase.getDriveIdForElement(element);
-
                 var channel = $(element).data('channel');
                 var type = $(element).data('type');
-                var opponentType = type == "switcher" ? "senser" : "switcher";
-                var opponent = $(element).closest("form").find("button[data-for='channel_type'][data-type=" + opponentType + "]");
-
-                $(element).click(function(event){
-                    $(element).prop("enabled", true);
-                    opponent.prop("disabled", true);
+                var opponentType = type == "switcher" ? "sensor" : "switcher";
+                var anotherChannel = channel == "A" ? "B" : "A";
+                var opponent = $(element).closest("form").find("button[data-for='channel_type'][data-channel='" + channel + "'][data-type=" + opponentType + "]");
+                var anotherTypeToActivate = $(element).closest("form").find("button[data-for='channel_type'][data-channel='" + anotherChannel + "'][data-type=" + opponentType + "]");
+                var anotherTypeToDeactivate = $(element).closest("form").find("button[data-for='channel_type'][data-channel='" + anotherChannel + "'][data-type=" + type + "]");
+                var typeAInput = $(element).closest("form").find("input[name='type_A']");
+                var typeBInput = $(element).closest("form").find("input[name='type_B']");
+                $(element).click(function (event) {
+                    //self.activateChannelType(driveId, channel, type);
+                    $(element).addClass("mdl-button--raised");
+                    opponent.removeClass("mdl-button--raised");
+                    anotherTypeToActivate.addClass("mdl-button--raised");
+                    anotherTypeToDeactivate.removeClass("mdl-button--raised");
+                    typeAInput.val(type);
+                    typeBInput.val(opponentType);
                     event.preventDefault();
+
                 })
             });
         },
-
         bindEvents: function () {
             drivesBase.bindEvents();
 
